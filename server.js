6382c245b59d80acdc316af8fe36deaf98591dbb
Aubrey Little <aubreylittle@iMac.local>
2023-10-24 14:26:27 -0400

    Server Hello World Complete> creating DB.

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get('/',(req,res) => {
+    res.send('Hello');
+
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+
+console.log(`Server is up and running on ${PORT}`) 
 });
