e4336d07ee415b7c5fc49a659fdb04badc0dce8a
Aubrey Little <aubreylittle@iMac.local>
2023-11-10 09:26:00 -0500

    Code Change

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
-const db = require('./db');
+const pg = require('pg');
 
-db.query('SELECT * FROM watches;', [], (error, results) => {
-  if (error) {
-    throw error;
-  }
-  console.log(results.rows);
+const client = new pg.Client('postgres://localhost:5433/WatchCollectionDB');
+console.log(client);
+
+client.connect((err) => {
+    if (err) {
+        console.log('Connection error', err.stack);
+    } else {
+        console.log('Connected to watch DB');
+    }
 });
+
+
+async function getAllWatches() {
+try {
+    const {rows: watches} = await client.query(`
+    SELECT * FROM watches;
+    
+    
+    `);
+    console.log(watches);
+} catch (error) {
+    console.error(error);
+}
+
+}
+
+
+
+const init = async () => {
+
+    try {
+        getAllWatches(); 
+    } catch (error) {
+        
+    }
+}
+init();
